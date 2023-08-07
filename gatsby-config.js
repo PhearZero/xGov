aef456aebf034dfca0239bca624bfd5e8654be92
Michael J Feher <github@phearzero.com>
2023-08-06 20:40:22 -0500

    ⚡ PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,42 @@
-// const {existsSync, symlinkSync} = require("fs");
-// const {resolve} = require("path");
-//
-// if(!existsSync('./src/pages/proposal')){
-//   symlinkSync(resolve('./Proposals'), resolve('./src/pages/proposal'))
-// }
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `cockney`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [
-    `gatsby-plugin-sass`,
-    {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "content",
-      "path": `${__dirname}/Proposals`
+    siteMetadata: {
+        title: `xGov`,
+        siteUrl: `https://xgov.algorand.foundation`
     },
-  },   {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-highlight-code`,
-          },
-        ],
-      },
-    },]
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                icon: `src/images/algorand.png`,
+                name: `xGov Voting`,
+                short_name: `xGov`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+            },
+        },
+
+        `gatsby-plugin-sass`,
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "content",
+                "path": `${__dirname}/Proposals`
+            },
+        }, {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-highlight-code`,
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+    ]
 };
